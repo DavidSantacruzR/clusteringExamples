bf5de0bf22d40131081fb12fb1e0459970789058
David S <axelsantacruzr@gmail.com>
2021-09-07 17:12:04 -0500

    Initial project datafiles.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Hierarchical Clustering Example
+A simple clustering algorithm using securities data.
+All the data have been generated randomly following a normal distribution.
+
+The clusters n=3 where selected using dendrogram where the clusters data
+inside each one is as similar as possible.
+
+In this example there are 3 main features of the data: volume, No. of trades
+and trading frequency. The features are reduced to two dimensions, each one important to determine de liquidity of a certain security.
+
+The dimensionality reduction follows a simple PCA.
---
+++ clusteringCode/clusteringHierarchicalExample.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import decomposition as dec
+from sklearn.cluster import AgglomerativeClustering
+import scipy.cluster.hierarchy as shc
+import os
+
+# Declaring my working directory:
+
+os.chdir('myWorkingDirectory')
+
+# Importing data:
+
+my_data: pd.DataFrame = pd.read_csv('data_test.csv', delimiter=';')
+print(my_data.head(10))
+number_of_features: int = len(my_data.columns)
+features_labels = my_data[1:number_of_features].columns.values.tolist()
+target_label = features_labels[0]  # Specifies the target label
+features_labels = features_labels[1:4]
+
+# Implementing a simple principal component analysis to reduce the features.
+
+pca = dec.PCA(n_components=2, svd_solver='full')
+principal_components = pca.fit_transform(my_data[features_labels])
+principal_dataframe: pd.DataFrame = pd.DataFrame(data=principal_components, columns=['component_1', 'component_2'])
+new_features = [target_label, 'component_1', 'component_2']
+
+# Defining the dataframe with the components before clustering
+
+labeled_principal_components: pd.DataFrame = pd.DataFrame(my_data.join(principal_dataframe))
+labeled_principal_components: pd.DataFrame = labeled_principal_components[new_features]
+
+# Estimating the clusters
+plt.figure(figsize=(10, 7))
+plt.title('dendrogram')
+dendrogram_figure = shc.dendrogram(shc.linkage(labeled_principal_components[new_features[1:2]], method='ward'))
+plt.show()
+
+cluster = AgglomerativeClustering(n_clusters=3, affinity='euclidean', linkage='ward')
+cluster.fit_predict(labeled_principal_components[new_features[1:2]])
+
+plt.figure(figsize=(10, 7))
+plt.scatter(labeled_principal_components['component_1'], labeled_principal_components['component_2'], c=cluster.labels_)
+plt.show()
+
+# Getting the final dataframe along with the clusters
+liquidity_groups: pd.DataFrame = pd.DataFrame(cluster.labels_, columns=['groups'])
+final_dataframe: pd.DataFrame = labeled_principal_components.join(liquidity_groups)
+print(final_dataframe.head(10))
